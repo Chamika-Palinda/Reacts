e95b8eb127d7b70d4beab432385057ad5decaf48
OfficialChamika <chamikarathnayake890@gmail.com>
2021-06-16 19:47:03 +0530

    Rename

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -77,7 +77,7 @@ export default function Chats() {
     <div className='chats-page'>
       <div className='nav-bar'>
         <div className='logo-tab'>
-          Unichat
+          Chatter
         </div>
 
         <div onClick={handleLogout} className='logout-tab'>
